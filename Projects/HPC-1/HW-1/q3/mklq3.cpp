942de9a64ce3ba1e0593a9ec7f1833fff08b504a
dhruvin <dhruvin1991@gmail.com>
2015-11-10 15:16:27 -0500

    HPC-HW1 aligned

--- Projects/HPC-1/HW-1/q3/mklq3.cpp
+++ Projects/HPC-1/HW-1/q3/mklq3.cpp
@@ -7,78 +7,78 @@
 #include<ctime>
 #include<mkl.h>
 int main(int argc, char* argv[])
- {
+{
 
-    int nodes[] = {2000000, 4000000,6000000,8000000,10000000};
-    int k = sizeof(nodes)/sizeof(nodes[0]);
+  int nodes[] = {2000000, 4000000,6000000,8000000,10000000};
+  int k = sizeof(nodes)/sizeof(nodes[0]);
 
-    double msecs[k];
+  double msecs[k];
 
-    clock_t start, end;
-    //std::cout << nodes <<"\n";
-    int a=0;
-    for(int z=0; z < k; z++)
+  clock_t start, end;
+  //std::cout << nodes <<"\n";
+  int a=0;
+  for(int z=0; z < k; z++)
     {
 
-    double dotproduct=0.0;
+      double dotproduct=0.0;
 
-    double* A = new double [nodes[z]];
-    double* B = new double [nodes[z]];
-	int len = nodes[z];
-	int incm = 1, incn = 1;
+      double* A = new double [nodes[z]];
+      double* B = new double [nodes[z]];
+      int len = nodes[z];
+      int incm = 1, incn = 1;
 
-    for(int i = 0; i< nodes[z]; i++)
-    {
+      for(int i = 0; i< nodes[z]; i++)
+	{
 
-	A[i] = 1;
-	B[i] = 2;
+	  A[i] = 1;
+	  B[i] = 2;
 
-    }
-    start = clock();
+	}
+      start = clock();
     
-	dotproduct = cblas_ddot(len, A, incm, B, incn);
+      dotproduct = cblas_ddot(len, A, incm, B, incn);
 
-    end = clock();
-    msecs[a] =  ((double) (end - start)) * 1000 / CLOCKS_PER_SEC ;
-    std::cout << msecs[a] <<"\n";
-    std::cout << dotproduct <<"\n";
-    a=a+1;
+      end = clock();
+      msecs[a] =  ((double) (end - start)) * 1000 / CLOCKS_PER_SEC ;
+      std::cout << msecs[a] <<"\n";
+      std::cout << dotproduct <<"\n";
+      a=a+1;
 
-    delete[] A ;
-    delete[] B ;
+      delete[] A ;
+      delete[] B ;
 
 
 
     }
 
-	std::ofstream write_file("mkl.dat");
-	// Write numbers as +x.<13digits>e+00 (width 20)
-	write_file.setf(std::ios::scientific);
-	write_file.precision(30);
+  std::ofstream write_file("mkl.dat");
+  // Write numbers as +x.<13digits>e+00 (width 20)
+  write_file.setf(std::ios::scientific);
+  write_file.precision(30);
 
-	assert(write_file.is_open());
-	for (int i=0; i< 5; i++)
-	{
+  assert(write_file.is_open());
+  for (int i=0; i< 5; i++)
+    {
 
-	write_file << msecs[i] << "\n";
+      write_file << msecs[i] << "\n";
 
-	}
-	write_file.close();
+    }
+  write_file.close();
 
-	std::ofstream write_v("nodes.dat");
-	// Write numbers as +x.<13digits>e+00 (width 20)
-	write_v.setf(std::ios::scientific);
-	write_v.precision(30);
+  std::ofstream write_v("nodes.dat");
+  // Write numbers as +x.<13digits>e+00 (width 20)
+  write_v.setf(std::ios::scientific);
+  write_v.precision(30);
 
-	assert(write_v.is_open());
-	for (int i=0; i< 5; i++)
-	{
+  assert(write_v.is_open());
+  for (int i=0; i< 5; i++)
+    {
 
-	write_v << nodes[i] <<  "\n"; 
+      write_v << nodes[i] <<  "\n"; 
 
-	}
-	write_v.close();
+    }
+  write_v.close();
 
 
-	    return 0;
-	    }
+  return 0;
+}
--- Projects/HPC-1/HW-1/q3/q3.cpp
+++ Projects/HPC-1/HW-1/q3/q3.cpp
@@ -1,88 +1,88 @@
-    #include<iostream>
-    #include<cmath>
-    #include<string>
-    #include<cassert>
-    #include<fstream>
-    #include<cstdlib>
-    #include<ctime>
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<cassert>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
     
-    int main(int argc, char* argv[])
+int main(int argc, char* argv[])
+{
+
+  int nodes[] = {2000000, 4000000,6000000,8000000,10000000};
+  int k = sizeof(nodes)/sizeof(nodes[0]);
+
+  double msecs[k];
+  clock_t start, end;
+    
+  int a=0;
+  for(int z=0; z < k; z++)
     {
 
-    int nodes[] = {2000000, 4000000,6000000,8000000,10000000};
-     int k = sizeof(nodes)/sizeof(nodes[0]);
+      double dotproduct=0.0;
 
-    double msecs[k];
-    clock_t start, end;
-    
-    int a=0;
-    for(int z=0; z < k; z++)
-    {
-
-    double dotproduct=0.0;
-
-   double* A = new double [nodes[z]];
-   double* B = new double [nodes[z]];
+      double* A = new double [nodes[z]];
+      double* B = new double [nodes[z]];
  
   
-    for(int i = 0; i< nodes[z]; i++)
-    {
+      for(int i = 0; i< nodes[z]; i++)
+	{
         
-        A[i] = 1;
-        B[i] = 2;
+	  A[i] = 1;
+	  B[i] = 2;
         
-    }
- start = clock();
-  // clock_gettime(CLOCK_MONOTONIC, &start); 
-    for(int i = 0; i< nodes[z]; i++)
-    {
+	}
+      start = clock();
+      // clock_gettime(CLOCK_MONOTONIC, &start); 
+      for(int i = 0; i< nodes[z]; i++)
+	{
         
-        dotproduct += A[i]*B[i];
+	  dotproduct += A[i]*B[i];
         
-    }
+	}
    
-  end = clock();
-   std::cout << dotproduct <<"\n";	
-   msecs[a] =  ((double) (end - start)) * 1000 / CLOCKS_PER_SEC ;
-  std::cout << msecs[a] <<"\n";
-  // 
-    a=a+1;
+      end = clock();
+      std::cout << dotproduct <<"\n";	
+      msecs[a] =  ((double) (end - start)) * 1000 / CLOCKS_PER_SEC ;
+      std::cout << msecs[a] <<"\n";
+      // 
+      a=a+1;
 
-    delete[] A ;
-    delete[] B ;
+      delete[] A ;
+      delete[] B ;
 
 
 
     }
 
-std::ofstream write_file("go.dat");
-// Write numbers as +x.<13digits>e+00 (width 20)
-write_file.setf(std::ios::scientific);
-write_file.precision(30);
+  std::ofstream write_file("go.dat");
+  // Write numbers as +x.<13digits>e+00 (width 20)
+  write_file.setf(std::ios::scientific);
+  write_file.precision(30);
 
-assert(write_file.is_open());
-for (int i=0; i< 5; i++)
-{
+  assert(write_file.is_open());
+  for (int i=0; i< 5; i++)
+    {
 
-write_file << msecs[i] << "\n";
+      write_file << msecs[i] << "\n";
 
-}
-write_file.close();
-
-std::ofstream write_v("nodes.dat");
-// Write numbers as +x.<13digits>e+00 (width 20)
-write_v.setf(std::ios::scientific);
-write_v.precision(30);
-
-assert(write_v.is_open());
-for (int i=0; i< 5; i++)
-{
-
-write_v << nodes[i] <<  "\n"; 
-
-}
-write_v.close();
-
-
-    return 0;
     }
+  write_file.close();
+
+  std::ofstream write_v("nodes.dat");
+  // Write numbers as +x.<13digits>e+00 (width 20)
+  write_v.setf(std::ios::scientific);
+  write_v.precision(30);
+
+  assert(write_v.is_open());
+  for (int i=0; i< 5; i++)
+    {
+
+      write_v << nodes[i] <<  "\n"; 
+
+    }
+  write_v.close();
+
+
+  return 0;
+}
