fb6c51e69ead2ace809a9111c6f5af8a52f72184
dhruvin <dhruvin1991@gmail.com>
2015-11-10 14:54:54 -0500

    grid generation

--- Projects/lid driven cavity flow/gridgeneration.h
+++ Projects/lid driven cavity flow/gridgeneration.h
@@ -8,29 +8,29 @@
 void gridgeneration(int nodes, double r,double** x,double** y,double* dx)
 {
 
-int nm = double(nodes)/double(2) + 0.5;
-int ne = nodes - nm;
+  int nm = double(nodes)/double(2) + 0.5;
+  int ne = nodes - nm;
 
  
-if (r==1)
-{
-*dx = 1./double(nodes-1); 
-double x1[nodes], y1[nodes];	
-x1[0] =0.0;
-y1[0] =0.0;
-for(int i=1; i< nodes; i++){
-x1[i] = x1[i-1] + *dx ;
-y1[i] = y1[i-1] + *dx;
-}
-for(int i = 0; i < nodes; i++)
-{
-for(int j = 0; j < nodes; j++){
+  if (r==1)
+    {
+      *dx = 1./double(nodes-1); 
+      double x1[nodes], y1[nodes];	
+      x1[0] =0.0;
+      y1[0] =0.0;
+      for(int i=1; i< nodes; i++){
+	x1[i] = x1[i-1] + *dx ;
+	y1[i] = y1[i-1] + *dx;
+      }
+      for(int i = 0; i < nodes; i++)
+	{
+	  for(int j = 0; j < nodes; j++){
 
-x[i][j] = x1[j];
-y[i][j] = y1[i];
-}
-}
+	    x[i][j] = x1[j];
+	    y[i][j] = y1[i];
+	  }
+	}
 
-//std::cout << *dx << "\n";
-}
+      //std::cout << *dx << "\n";
+    }
 }
